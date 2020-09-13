cf5581f47d06927158bf939cb695330de73cebbf
Timothée Couble <timothee.couble@gmail.com>
2020-09-13 17:11:26 +0200

    Finish-up contact form

--- src/components/input.js
+++ src/components/input.js
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
 
-// import classnames from "classnames";
+import classnames from "classnames";
 import css from "./input.module.css";
 
-const Input = ({
-  form,
-  setForm,
-  title,
-  text,
-  required = false,
-  type = "input",
-}) => {
+const Input = ({ form, setForm, title, text, isRequired, type = "input" }) => {
   const keyName = title.toLowerCase();
 
   const handleChange = (event) => {
     setForm({ ...form, [keyName]: event.target.value });
   };
 
+  const textInput = classnames(css.textInput, { [css.required]: isRequired });
+  const textArea = classnames(css.textArea, { [css.required]: isRequired });
+
   return (
     <div className={css.input}>
-      <span className={css.title}>{title + " " + (required ? "*" : "")}</span>
+      <span className={css.title}>{title + " *"}</span>
       {type === "input" && (
         <input
-          className={css.textInput}
+          className={textInput}
           value={form[keyName]}
           placeholder={text}
           onChange={handleChange}
@@ -30,7 +26,7 @@ const Input = ({
       )}
       {type === "textarea" && (
         <textarea
-          className={css.textArea}
+          className={textArea}
           value={form[keyName]}
           placeholder={text}
           onChange={handleChange}
--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -1,36 +1,61 @@
 import React, { useState } from "react";
 import { useIntl } from "react-intl";
-// import classnames from "classnames";
+import { Redirect } from "react-router-dom";
 
 import Form from "../components/input";
 import css from "./contact-me.module.css";
 
+const defaultForm = {
+  date: Date.now(),
+  name: "",
+  email: "",
+  subject: "",
+  message: "",
+};
+
 const ContactMe = ({ contact }) => {
   const intl = useIntl();
-  const [form, setForm] = useState({
-    date: Date.now(),
-    name: "",
-    email: "",
-    phone: "",
-    subject: "",
-    message: "",
-  });
+  const [form, setForm] = useState(defaultForm);
+  const [trySubmit, setTrySubmit] = useState(false);
+  const [redirect, setRedirect] = useState(false);
 
-  const handleSubmit = () => {};
+  const isRequired = (title, submitting = false) => {
+    const key = title.toLowerCase();
+    return trySubmit || submitting ? form[key] === "" : false;
+  };
 
+  const handleSubmit = (e) => {
+    const missingKey = intl.messages.contact.inputs.findIndex((elem) =>
+      isRequired(elem.title, true)
+    );
+    const fieldsCompleted = missingKey === -1;
+
+    if (fieldsCompleted) setForm(defaultForm);
+    setTrySubmit(fieldsCompleted ? false : true);
+    setRedirect(fieldsCompleted ? true : false);
+  };
+
+  if (redirect) return <Redirect to="/" />;
   return (
     <div className={css.page}>
-      <div className={css.contact}>
+      <div className={css.form}>
         <div className={css.title}>{intl.messages.contact.title}</div>
-        <div className={css.form}>
-          {intl.messages.contact.inputs.map((input, index) => {
-            return (
-              <Form form={form} setForm={setForm} {...input} key={index} />
-            );
-          })}
-          <div className={css.submit} onClick={handleSubmit}>
-            {intl.messages.contact.submit}
-          </div>
+        {intl.messages.contact.inputs.map((input, index) => {
+          return (
+            <Form
+              form={form}
+              setForm={setForm}
+              isRequired={isRequired(input.title)}
+              {...input}
+              key={index}
+            />
+          );
+        })}
+        <p className={css.requiredMessage}>
+          {"* " + intl.messages.contact.required}
+        </p>
+        <div className={css.submit} onClick={handleSubmit}>
+          {intl.messages.contact.submit}
         </div>
       </div>
     </div>

f59bd304e94835e338fbefc9a409d258fbc622f7
Timothée Couble <timothee.couble@gmail.com>
2020-10-17 01:31:20 +0200

    Init project + english update

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState }  from "react";
 import { useIntl } from "react-intl";
 
+import classnames from "classnames";
 import css from "./css/projects.module.css";
 
 const Projects = ({ projects }) => {
   const intl = useIntl();
+  const [open, setOpen] = useState(false);
+
+  const onClick = (status) => (e) => {
+    setOpen(status);
+  };
+
+  const button = classnames(css.button, {[css.hide]: open})
+  const background = classnames(css.background, {[css.extendedBackground]: open})
 
   return (
     <div className={css.projectsPage}>
-      <div className={css.background}>
-        <div className={css.button}>{intl.messages.body.projects_button}</div>
+      <div className={background}>
+        <div className={button} onClick={onClick(true)}>{intl.messages.body.projects_button}</div>
+        {open && <h2 className={css.title}>{intl.messages.body.projects}</h2>}
+        {open && <div className={css.projectScreen}></div>}
       </div>
     </div>
   );
